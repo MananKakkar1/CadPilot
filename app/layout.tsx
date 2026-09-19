99625cd91ee7d4c007400f5ef33b72e2501c8d9b
kakkarm2 <manan.kakkar.2005@outlook.com>
2026-09-19 15:16:28 -0400

    Run CAD worker with local environment and add project viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import './magic-overrides.css';
 import './reference.css';
 import './chili-editor.css';
 import './agent-workspace.css';
+import './project-viewport.css';
 import './auth.css';
 import 'katex/dist/katex.min.css';
 import './chili-editor.css';
